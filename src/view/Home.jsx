8533f2a9c0f5dd295b83cedbfad9572fea5e2849
Will Holz <holz.willian@gmail.com>
2024-11-07 23:02:11 -0300

    mudando a home

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,84 +1,60 @@
 import React from "react";
-import Button from "../components/button";
 import styles from "../styles/Home.module.css";
 import logo from "../images/Logo_pacato_verde.png";
 import { Link } from "react-router-dom";
-
-import {
-  FaDumbbell,
-  FaFutbol,
-  FaHospital,
-  FaBook,
-  FaUser,
-  FaMapMarkerAlt,
-  FaHeart,
-} from "react-icons/fa";
+import { FaDumbbell, FaFutbol, FaHospital, FaBook } from "react-icons/fa";
 
 const Home = () => {
   const handleClick = (servico) => {
-    console.log("Voce apertou no botão");
+    console.log("Você apertou no botão");
   };
+
   return (
     <div className={styles.homeContainer}>
-      <img src={logo} alt="Logo Pacato cidadão" className={styles.logo}></img>
+      <img src={logo} alt="Logo Pacato cidadão" className={styles.logo} />
 
       <div className={styles.iconContainer}>
         <Link to="/fisica">
-          <button className={styles.iconButton} onClick={() => handleClick()}>
-            <div className={styles.iconHome}>
-              <FaDumbbell size={40} />{" "}
-            </div>
+          <button
+            className={styles.iconButton}
+            onClick={() => handleClick("Exercício")}
+          >
+            <FaDumbbell size={40} />
           </button>
         </Link>
 
         <Link to="/esporte">
-          <button className={styles.iconButton} onClick={() => handleClick()}>
-            <div className={styles.iconHome}>
-              <FaFutbol size={40} />
-            </div>
+          <button
+            className={styles.iconButton}
+            onClick={() => handleClick("Esporte")}
+          >
+            <FaFutbol size={40} />
           </button>
         </Link>
 
-        <Link to="saude">
-          <button className={styles.iconButton} onClick={() => handleClick()}>
-            <div className={styles.iconHome}>
-              <FaHospital size={40} />{" "}
-            </div>
+        <Link to="/saude">
+          <button
+            className={styles.iconButton}
+            onClick={() => handleClick("Saúde")}
+          >
+            <FaHospital size={40} />
           </button>
         </Link>
 
-        <Link to="educacao">
-          <button className={styles.iconButton} onClick={() => handleClick()}>
-            <div className={styles.iconHome}>
-              <FaBook size={40} />{" "}
-            </div>
+        <Link to="/educacao">
+          <button
+            className={styles.iconButton}
+            onClick={() => handleClick("Educação")}
+          >
+            <FaBook size={40} />
           </button>
         </Link>
+      </div>
 
-        <button className={styles.iconButton} onClick={() => handleClick()}>
-          <div className={styles.iconHome}>
-            <FaUser size={40} />{" "}
-          </div>
-        </button>
-
-        <button className={styles.iconButton} onClick={() => handleClick()}>
-          <div className={styles.iconHome}>
-            <FaMapMarkerAlt size={40} />{" "}
-          </div>
-        </button>
-
-        <div className={styles.iconHome}>
-          <Button>
-            {" "}
-            <FaHeart size={40} />{" "}
-          </Button>
-        </div>
-
-        <div className={styles.buttonContainer}>
-          <Link to="/todos">
-            <Button label="VER TODOS OS SERVIÇOS" />
-          </Link>
-        </div>
+      <div className={styles.buttonContainer}>
+        <Link to="/todos">
+          <button className={styles.button}>VER TODOS OS SERVIÇOS</button>
+        </Link>
       </div>
     </div>
   );
